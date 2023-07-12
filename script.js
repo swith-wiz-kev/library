451fb8fb46337818eedfe0413cf0154d65b21b08
kev <ddrain10@gmail.com>
2023-07-12 10:42:18 +0800

    Add display book functions

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function addBookToLibrary() {
   );
   myLibrary.push(newBook);
 }
+
+function displayBooks() {
+  const cardGrid = document.querySelector(".cardGrid");
+  for (let i = 0; i < myLibrary.length; i++) {
+    addCard(i, cardGrid);
+  }
+}
+
+function addCard(i, parentElement) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  addLineInfo(myLibrary[i].title, "Title: ", card, "cardText");
+  addLineInfo(myLibrary[i].author, "Author: ", card, "cardText");
+  addLineInfo(myLibrary[i].pages, "Pages: ", card, "cardText");
+  addLineInfo(myLibrary[i].read, "Read: ", card, "cardText");
+  parentElement.appendChild(card);
+}
+
+function addLineInfo(contentText, labelText, parentElement, elementClass) {
+  const element = document.createElement("div");
+  element.classList.add(elementClass);
+  element.textContent = labelText + contentText;
+  parentElement.appendChild(element);
+}
