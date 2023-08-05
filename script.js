6a9a190ba6bf83bc2b7ad1e82e6a926450b7068e
kev <ddrain10@gmail.com>
2023-08-05 13:08:10 +0800

    Add Remove Book button

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const authorField = document.querySelector("#author");
 const pagesField = document.querySelector("#pages");
 const readField = document.querySelector("#read");
 
-function Book(title, author, pages, read, entryNumber) {
+function Book(title, author, pages, read, entryNumberLocal) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.entryNumber = entryNumber;
+  this.entryNumber = entryNumberLocal;
 }
 
 function addBookToLibrary(e) {
@@ -51,12 +51,30 @@ function addCard(i, parentElement) {
     const card = document.createElement("div");
     card.dataset.entryNumber = myLibrary[i].entryNumber;
     card.classList.add("card");
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "X";
+    removeButton.classList.add("cardRemove");
+    removeButton.dataset.entryNumberButton = myLibrary[i].entryNumber;
+    removeButton.addEventListener("click", removeCard);
+    card.appendChild(removeButton);
     addLineInfo(myLibrary[i].title, "Title: ", card, "cardText");
     addLineInfo(myLibrary[i].author, "Author: ", card, "cardText");
     addLineInfo(myLibrary[i].pages, "Pages: ", card, "cardText");
     addLineInfo(myLibrary[i].read, "Read: ", card, "cardText");
     parentElement.appendChild(card);
   }
+}
+
+function removeCard(e) {
+  const targetEntryNumber = e.target.dataset.entryNumberButton;
+  const targetElement = document.querySelector(
+    `[data-entry-number="${targetEntryNumber}"]`
+  );
+  targetElement.remove();
+  const targetLibraryIndex = myLibrary.findIndex(
+    (book) => book.entryNumber == targetEntryNumber
+  );
+  myLibrary.splice(targetLibraryIndex, 1);
 }
 
 function addLineInfo(contentText, labelText, parentElement, elementClass) {
@@ -84,7 +102,5 @@ function initializeClicks() {
   closeFormButton.addEventListener("click", closeForm);
   addBookButton.addEventListener("click", addBookToLibrary);
 }
-
-function deleteCard(params) {}
 
 initializeClicks();
